5304eab63d30f8fab4c85d825fddeb493aa20542
Raphael Wintrich <raphael.wintrich@gmail.com>
2017-04-15 21:19:48 +0200

    fix login2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,46 @@
 <template>
-  <main-layout>
-    <p>Login</p>
-    <button @click="login()" class="primary big">Login</button>
-  </main-layout>
+  <q-layout>
+
+    <!-- HEADER -->
+    <slot name="header">
+      <div slot="header" class="toolbar">
+        <button class="hide-on-drawer-visible" @click="$refs.leftDrawer.open()">
+          <i>menu</i>
+        </button>
+        <q-toolbar-title :padding="1">
+          Login
+        </q-toolbar-title>
+
+      </div>
+    </slot>
+    <!-- HEADER END -->
+
+    <!-- MAIN -->
+    <div class="layout-view">
+      <div class="layout-padding">
+        <p icon="lock">FS Login</p>
+        <button @click="login()" class="primary big">Login</button>
+      </div>
+    </div>
+    <!-- MAIN END -->
+
+
+
+    <router-view class="layout-view"></router-view>
+
+
+
+    <!--
+    <div slot="footer" class="toolbar">
+      <div class="auto flex justify-center within-iframe-hide">
+
+      </div>
+      <q-toolbar-title :padding="0" class="within-iframe-only">
+        Footer
+      </q-toolbar-title>
+    </div>
+    -->
+  </q-layout>
 </template>
 
 <script>
