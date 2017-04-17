12cb5b8edac08094419e4c46bc1c161faab1ccb7
Matthias Larisch <mail@matthias-larisch.de>
2017-04-17 15:10:19 +0200

    Show stores only when existing

--- src/components/Stores.vue
+++ src/components/Stores.vue
@@ -3,21 +3,23 @@
     <main-layout>
 
       <!-- MAIN -->
-      <h6>Deine Betriebe</h6>
-      <div class="card">
-        <div class="list no-border">
-          <router-link :to="'/store/' + store.id" tag="div" class="item two-lines item-link" v-for="store in stores" :key="store.id">
-            <div class="item-primary">
-              <i>store</i>
-            </div>
-            <div class="item-content">
-              <div>{{ store.name }}</div>
-              <div>
-                {{ store.street }} {{ store.streetNumber }}, {{ store.zip }} {{ store.city }}
+      <div v-if="stores">
+        <h6>Deine Betriebe</h6>
+        <div class="card">
+          <div class="list no-border">
+            <router-link :to="'/store/' + store.id" tag="div" class="item two-lines item-link" v-for="store in stores" :key="store.id">
+              <div class="item-primary">
+                <i>store</i>
               </div>
-            </div>
-          </router-link>
-          <hr class="inset">
+              <div class="item-content">
+                <div>{{ store.name }}</div>
+                <div>
+                  {{ store.street }} {{ store.streetNumber }}, {{ store.zip }} {{ store.city }}
+                </div>
+              </div>
+            </router-link>
+            <hr class="inset">
+          </div>
         </div>
       </div>
       <!-- MAIN END -->
@@ -32,7 +34,7 @@
   export default {
     data () {
       return {
-        stores: []
+        stores: null
       }
     },
     methods: {
