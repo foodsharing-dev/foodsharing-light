1c82b2d1bdc42444561759e9f74ace3cf6140f7b
Matthias Larisch <mail@matthias-larisch.de>
2017-04-18 01:34:11 +0200

    Add forgotten formattedDate

---
+++ src/components/FormattedDate.vue
@@ -0,0 +1,13 @@
+<script>
+  import moment from 'moment'
+  export default {
+    props: {
+      date: {
+        required: true
+      }
+    },
+    render (h) {
+      return h('span', {}, moment(this.date).calendar())
+    }
+  }
+</script>
