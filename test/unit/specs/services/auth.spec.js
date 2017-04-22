f89fc296018ff1be05a32ce3d78b19efa47c402d
Nick Sellen <git@nicksellen.co.uk>
2017-04-22 19:40:06 +0200

    Add more tests for services/auth

--- test/unit/specs/services/auth.spec.js
+++ test/unit/specs/services/auth.spec.js
@@ -1,15 +1,54 @@
+import auth from 'services/auth'
+import api from 'services/api'
+import foodsharing from 'services/foodsharing'
+
+const sandbox = sinon.sandbox.create()
+
 describe('services/auth', () => {
-  let auth
+  afterEach(() => sandbox.restore())
 
   beforeEach(() => {
-    let path = require.resolve('services/auth')
-    if (require.cache[path]) {
-      delete require.cache[path]
-    }
-    auth = require('services/auth').default
+    // TOD: should probably be a reset function inside services/auth
+    Object.assign(auth.state, {
+      authenticated: false,
+      to: null,
+      user: null
+    })
   })
 
-  it('is initialized as unauthenticated', () => {
-    expect(auth.state.authenticated).to.be.false
+  describe('check', () => {
+    it('checks login for api and foodsharing', () => {
+      let user = { id: 'user1', firstName: 'Peter' }
+      let apiStub = sandbox.stub(api, 'checkLogin').resolves(user)
+      let foodsharingStub = sandbox.stub(foodsharing, 'checkLogin').resolves(true)
+      return auth.check().then(() => {
+        expect(apiStub).to.have.been.called
+        expect(foodsharingStub).to.have.been.called
+        expect(auth.state.user).to.deep.equal(user)
+        expect(auth.state.authenticated).to.be.true
+      })
+    })
+
+    it('does not check foodsharing if api is not logged in', () => {
+      let apiStub = sandbox.stub(api, 'checkLogin').resolves(null)
+      let foodsharingStub = sandbox.stub(foodsharing, 'checkLogin').resolves(true)
+      return auth.check().then(() => {
+        expect(apiStub).to.have.been.called
+        expect(foodsharingStub).to.not.have.been.called
+        expect(auth.state.user).to.be.null
+        expect(auth.state.authenticated).to.be.false
+      })
+    })
+
+    it('will not assign user if foodsharing check fails', () => {
+      let apiStub = sandbox.stub(api, 'checkLogin').resolves({ id: 1 })
+      let foodsharingStub = sandbox.stub(foodsharing, 'checkLogin').resolves(false)
+      return auth.check().then(() => {
+        expect(apiStub).to.have.been.called
+        expect(foodsharingStub).to.have.been.called
+        expect(auth.state.user).to.be.null
+        expect(auth.state.authenticated).to.be.false
+      })
+    })
   })
 })
