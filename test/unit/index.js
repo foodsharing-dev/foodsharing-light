505d5d75c2f7eb3830aa0e17919def0be86103f0
Nick Sellen <git@nicksellen.co.uk>
2017-05-02 23:38:00 +0100

    Silence vue during tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 Vue.config.productionTip = false
+Vue.config.silent = true
 
 // require all test files (files that ends with .spec.js)
 export const testsContext = require.context('./specs', true, /\.spec$/)
