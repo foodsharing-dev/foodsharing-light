ed0b4023cebd6cd17f7d96ebcfc2eab469ec56d1
Nick Sellen <git@nicksellen.co.uk>
2017-07-02 19:23:09 +0200

    Update deploy script to include set -e and git sha

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
-if quasar build
-then
+#!/bin/bash
+
+set -e
+
+if quasar build; then
+
+  # make current commit sha available publically
+  git rev-parse HEAD > dist/COMMIT
+
   echo "Build complete, deploying..."
   rsync -av --delete --safe-links dist/ fslight@foodsharing.de:/var/www/fslight/www/foodsharing-light/
   echo "Deploy complete!"
+
 fi
